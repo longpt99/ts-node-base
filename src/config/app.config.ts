08681fec43fd938c8bf7079ce93f994e1f17d282
longpt99 <longpt@vmodev.com>
2022-06-22 15:21:08 +0700

    Update cicd

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@ import { AppEnvironment } from '../common/interfaces';
 import { AppConfig } from '../common/interfaces/app-config.interface';
 
 const setupEnvironment = (): AppEnvironment => {
-  const mode = process.env.NODE_ENV ?? 'local';
+  const mode = process.env.NODE_ENV ?? 'production';
   return require(`./environments/${mode}.env`).ENV;
 };
 
